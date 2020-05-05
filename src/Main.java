a905d965e59aeb04e36b6cf5bfc6d212bcded7dd
pavelnazaro <pavelnazaro@mail.ru>
2020-05-05 16:50:29 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Epam!");
+        String str_Hello = "Hello";
+        String str_Epam = "Epam";
+        System.out.println(str_Hello + " " + str_Epam + "!");
     }
 }
